da19ecfaf5ebafe89a5ecdc5cf37ff87cfc81eb7
theskhan <suhelkhan06@gmail.com>
2016-06-10 13:14:06 +0530

    hmr

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
         <div>
         Home 1 1 1
+
+        <button className="btn btn-default">Hello World</button>
         </div>
     );
   }
